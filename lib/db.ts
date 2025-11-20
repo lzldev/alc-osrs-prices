2084fe98e2621a0d195d2cd7f3c1ebea9cf4efa1
Lucas Costa <francisco.lucas0507@gmail.com>
2025-11-20 13:47:34 -0300

    fix: reuse drizzle connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,23 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import postgres, { type Sql } from 'postgres'
 
 import * as auth_schema from '../auth-schema'
 
-const sql = postgres(process.env.POSTGRES_URL!)
+let connection: Sql<any>
+
+if (process.env.NODE_ENV === 'production') {
+  connection = postgres(process.env.POSTGRES_URL!)
+} else {
+  const globalConnection = global as typeof globalThis & {
+    connection: Sql<any>
+  }
+  if (!globalConnection.connection)
+    globalConnection.connection = postgres(process.env.POSTGRES_URL!)
+
+  connection = globalConnection.connection
+}
 
 // Connect to  Postgres
-export const db = drizzle(sql, {
+export const db = drizzle(connection, {
   schema: { ...auth_schema },
 })
